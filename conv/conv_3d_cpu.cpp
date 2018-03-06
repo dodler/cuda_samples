7d0b843576082e79c8b49535d7cb1c1dc45ac7a2
dodler <tntlagf93@mail.ru>
2018-03-07 01:04:41 +0300

    cpu convolution ready

--- conv/conv_3d_cpu.cpp
+++ conv/conv_3d_cpu.cpp
@@ -101,67 +101,98 @@ void deleteTensor3(tensor3 t, int d1,int d2, int d3){
 }
 
 tensor3 pad(tensor3 in, int cols, int rows, int depth, int pad){
+
 	int seq = 0;
 
 	int rCols = cols + 2 * pad;
 	int rRows = rows + 2 * pad;
 	int rd = depth + 2 * pad;
 
-	tensor3 res = new int**[rCols];
+	cout << "padding result:" << rCols << " " << rRows << " " << rd << endl;
 
-	for(int i = 0; i<rCols; i++){
-		res[i] = new int*[rRows];
-		for(int j = 0; j<rRows; j++){
-			res[i][j] = new int[rd];
-			for(int k = 0; k<rd; k++){
-				if (i == 0 || j == 0 || k == 0 || i == rCols-1 || j == rRows-1 || k == rd-1){
-					res[i][j][k] = 0;
-				}else{
-					res[i][j][k] = in[i-pad][j-pad][k-pad];
-				}
+	tensor3 res = initVals(rCols, rRows, rd, 0);
+
+	for(int i = pad; i<rCols - pad; i++){
+		for(int j = pad; j<rRows - pad; j++){
+			for(int k = pad; k<rd - pad; k++){
+				res[i][j][k] = in[i-pad][j-pad][k-pad];
 			}
 		}
-	} // fixme
+	}
 	return res;
 }
 
-tensor3 conv_3d(tensor3 in, tensor3 kernel, int cols,int rows, int depth, int kCols,int kRows,int kd, int stride, int pad){
-	tensor3 out = initVals(
-			(cols + 2 * pad - kCols) / stride + 1,
-			(rows + 2 * pad - kRows) / stride + 1,
-			(depth + 2 * pad - kd) / stride + 1);
+tensor3 conv_3d(tensor3 in, tensor3 kernel, int cols,int rows, int depth, int kCols,int kRows,int kd, int stride, int padding){
 
-	tensor3 inPad = pad(in,pad);
+	if ((cols + 2 * padding - kCols) % stride != 0){
+		cout << "bad stride" << endl;
+	}
+
+	if ((rows + 2 * padding - kRows) % stride != 0){
+		cout << "bad stride" << endl;
+	}
+
+	if ((depth + 2 * padding - kd) % stride != 0){
+		cout << "bad stride" << endl;
+	}
+
+	int rCols = (cols + 2 * padding - kCols) / stride + 1;
+	int rRows = (rows + 2 * padding - kRows) / stride + 1;
+	int rDepth = (depth + 2 * padding - kd) / stride + 1;
+
+	cout << (cols + 2 * padding - kCols) / stride + 1 << endl;
+	cout << (rows + 2 * padding - kRows) / stride + 1 << endl;
+	cout << (depth + 2 * padding - kd) / stride + 1  << endl;
+
+	tensor3 out = initVals(
+			(cols + 2 * padding - kCols) / stride + 1,
+			(rows + 2 * padding - kRows) / stride + 1,
+			(depth + 2 * padding - kd) / stride + 1,
+			0);
+
+	tensor3 inPad = pad(in, cols, rows, depth, padding);
 
 	int m = 0, n = 0, l = 0;
 
-	for(int i = 0; i<cols; i+= stride, m++){
-		for(int j = 0; j<rows; j+= stride, n++){
-			for(int k = 0; k<depth; k++, l++){
+	for(int i = 0; m<rCols; i+= stride, m++){
+		for(int j = 0; n<rRows; j+= stride, n++){
+			for(int k = 0; l<rDepth; k+=stride, l++){
 				int t = convolve(inPad, kernel, i,j,k, kCols, kRows, kd);
 				out[m][n][l] = t;
 			}
+			l=0;
 		}
+		n=0;
 	}
 	return out;
 }
 
 
 int main(int argc, char** argv){
-	tensor3 test = initSeq(27);
-	tensor3 large = initSeq(20);
+	tensor3 test = initSeq(3);
+
+	tensor3 large = initSeq(27);
 	tensor3 kernel = initVals(3,2);
+
 	printSlice(kernel, 0,3,3);
+
 	tensor3 p = pad(kernel, 3,3,3, 1);
 
-	printSlice(p, 1, 5,5);
+	//printSlice(p, 0, 5,5);
 
+	cout << "starting convolution" << endl;
 	int c = convolve(test,kernel, 0,0,0,3,3,3);
-	tensor3 con = conv_3d(large, kernel, 27,27,27,3,3,3,3,3);
+	tensor3 con = conv_3d(large, kernel, 27,27,27,3,3,3,1,0);
+
+	cout << "convolution done" << endl;
+
+	printSlice(test,2,3,3);
+	printSlice(con, 2, 11,11);
 
 	cout << c << endl;
 
+	deleteTensor3(con, 11,11,11);
 	deleteTensor3(p, 5,5,5);
-	deleteTensor3(test, 3,3,3);
+	deleteTensor3(test, 27,27,27);
 	deleteTensor3(kernel, 3,3,3);
 }
